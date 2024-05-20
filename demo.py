2485499246e6e9bdff3e67d047e829da44ef81b3
DamingW <daming021699@gmail.com>
2024-05-20 14:12:42 +0800

    add time latency record

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
+from builtins import print
 import os
 import sys
 import json
+import time
 
 from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
@@ -20,21 +22,24 @@ if __name__=='__main__':
     import warnings
     warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
 
+    #input image
     image_file = "/root/LLaVA_SU/images_su/lsu/smoke1.jpg"
 
-    # 检查 image 路径是否存在
+    # check image 
     if not os.path.exists(image_file):
         print(f"Error: Image file {image_file} does not exist.")
-        sys.exit(1)  # 结束运行
+        sys.exit(1) 
 
-    # 定义 args
+    # define input args
     args = type('Args', (), {
-        "model_path": "liuhaotian/llava-v1.5-13b",
+        "model_path": "liuhaotian/llava-v1.5-7b",
         "model_base": None,
-        "model_name": get_model_name_from_path("liuhaotian/llava-v1.5-13b"),
+        "model_name": get_model_name_from_path("liuhaotian/llava-v1.5-7b"),
+        # liuhaotian/llava-v1.5-7b
+        # liuhaotian/llava-v1.5-13b
         "query": None,
         "conv_mode": None,
-        "image_file": image_file,
+        "image_file": None,
         "sep": ",",
         "temperature": 0.2,
         "top_p": 0.99,
@@ -42,22 +47,29 @@ if __name__=='__main__':
         "max_new_tokens": 16
     })()
 
+    # check image 
+    if not os.path.exists(args.image_file):
+        print(f"Error: Image file {args.image_file} does not exist.")
+        sys.exit(1) 
+    
     # load pre-trained model
     model_name = get_model_name_from_path(args.model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(
         args.model_path, args.model_base, model_name)
-    
-    print(f' ** finish loading model {model_name} ** \n')
+    print(f'\n ** finish loading model {model_name} ** \n')
 
+    # load query prompt
     with open('/root/LLaVA_SU/lib/prompt.json','r') as f:
         query_dict=json.load(f)
-
     print(f'query_dict:\n{query_dict}\n')
     
-    
-
     # inference with returning a dict of answer text
-    answer_dict=su_inference(args, model_name, tokenizer, model, image_processor, image_file, query_dict)
+    start_time = time.time()
+    answer_dict=su_inference(args, model_name, tokenizer, model, image_processor, args.image_file, query_dict)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"\nsu_inference within: {execution_time:.2f} sec")
+
     print(f'answer_dict:\n{answer_dict}\n')
     # print(logits)
 
